129c3c19b5687a5e33b9decfecd9e583aef60ed5
lukeskins <luke@lukeskinner.me>
2017-01-23 22:51:47 -0800

    Cleaned code :art:

--- Algorithms/5_diagonalDifference.java
+++ Algorithms/5_diagonalDifference.java
@@ -8,34 +8,34 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int a[][] = new int[n][n];
-        for(int a_i=0; a_i < n; a_i++){
-            for(int a_j=0; a_j < n; a_j++){
-                a[a_i][a_j] = in.nextInt();
-            }
+        for(int a_i=0; a_i < n; a_i++) {
+                for(int a_j=0; a_j < n; a_j++) {
+                        a[a_i][a_j] = in.nextInt();
+                }
         }
 
         int first = 0;
         int second = 0;
 
         // calculate the first diagonal
-       for (int row = 0; row < n; row++) {
-           int col = row;
-           first += a[row][col];
-       }
-       // calculate the second diagonal
-       int secondCol = n - 1;
-       for (int row = 0; row < n; row++) {
-           second += a[row][secondCol];
-           secondCol--;
-       }
+        for (int row = 0; row < n; row++) {
+                int col = row;
+                first += a[row][col];
+        }
+        // calculate the second diagonal
+        int secondCol = n - 1;
+        for (int row = 0; row < n; row++) {
+                second += a[row][secondCol];
+                secondCol--;
+        }
 
         int diff = first - second;
         diff = Math.abs(diff);
 
-       System.out.println(diff);
-    }
+        System.out.println(diff);
+}
 }
